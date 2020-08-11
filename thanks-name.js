6a887a29877afe991f898b65ad514414ab20c141
Anthony Salamin <anthony.salamin@gmail.com>
2020-08-11 15:42:37 +0200

    Create thanks-name.js

---
+++ thanks-name.js
@@ -0,0 +1,13 @@
+/*
+ * Thanks
+ * CODEPEN: Scorpios | Thanks v.1
+ * BUILD: 31.07.2020 | anthonysalamin.ch
+*/
+(function thanks() {
+    const log = console.log,
+          url = window.location.href, // "https://www.scorpiosmykonos.com/thanks?name={{name}}"
+          injectName = document.getElementsByClassName("thanks-name")[0],
+          name = url.split("=")[1];
+    log(name);
+    injectName.textContent = name;
+  })();
