9d3076d29b774f3260b4cbe24d7ad3d5e7ae0d5c
Anthony Salamin <37147870+anthonysalamin@users.noreply.github.com>
2021-11-01 13:52:32 +0100

    Update email-validation.js

--- email-validation.js
+++ email-validation.js
@@ -7,7 +7,7 @@
 // on DOM loaded
 document.addEventListener("DOMContentLoaded", () => {
   emailValidation();
-  prettyLog(`loaded`, `V.2 | 30.08.2021 @ 18:00 | email validation`);
+  prettyLog(`success`, `loaded`, `V.2 | 30.08.2021 @ 18:00 | email validation`);
 }); // end DOM loaded
 
 // email validation
@@ -76,21 +76,21 @@ function emailValidation() {
         if (value.length >= 1) {
           if (arobase) {
             if (extension) {
-              prettyLog(`success`, `email seems legit`);
+              prettyLog(`success`, `ok`, `email seems legit`);
               // checkInput.style.color = colorValid;
               // return "email seems legit";
             } else {
-              prettyLog(`warning`, `email not yet valid`);
+              prettyLog(`warning`, `warning`, `email not yet valid`);
               // checkInput.style.color = colorInvalid;
               // return "email not yet valid";
             } // end if extension
           } else {
-            prettyLog(`warning`, `email missing "@" symbol`);
+            prettyLog(`warning`, `warning`, `email missing "@" symbol`);
             // checkInput.style.color = colorInvalid;
             // return 'email missing "@" symbol';
           } // end if arobase
         } else {
-          prettyLog(`error`, `email invalid, try again`);
+          prettyLog(`error`, `error`, `email seems invalid, try again`);
           // checkInput.style.color = colorInvalid;
           // return "email invalid, try again";
         } // end if value > 1
@@ -103,29 +103,26 @@ function emailValidation() {
 } // end emailValidation()
 
 // log helper
-function prettyLog(status, message) {
+function prettyLog(status, hint, message) {
   let color;
   switch (status) {
-      case "info":
-      color = "#4DD0E1";
+    case "info":
+      color = "#3498DB";
       break;
     case "success":
-      color = "#1DE9B6";
-      break;
-    case "loaded":
-      color = "#1DE9B6";
+      color = "#27AE60";
       break;
     case "warning":
-      color = "#FFC107";
+      color = "#E67E22";
       break;
     case "error":
-      color = "#FF3D00";
+      color = "#E74C3C";
       break;
     default:
       color = "#90A4AE";
   }
   console.log(
-    `%c${status}`,
+    `%c${hint}`,
     [
       `background: ${color}`,
       `border-radius: 0.5em`,
