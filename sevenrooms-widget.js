da0b1b9cf63817caaeb71899f56dc0326ff48466
Anthony Salamin <37147870+anthonysalamin@users.noreply.github.com>
2022-04-09 16:57:41 +0200

    added pretty log for sevenrooms widget

--- sevenrooms-widget.js
+++ sevenrooms-widget.js
@@ -1,13 +1,25 @@
 /*
- * 🟢 SCORPIOS | SevenroomsWidget v.2
- * last build 09.04.22 @ 13:19
-*/
+ * 🟢 SCORPIOS | sevenrooms-widget | v.2
+ * build 09.04.22 @ 13:19
+ * © 2022 anthonysalamin.ch
+ */
+
 document.addEventListener("DOMContentLoaded", () => {
+  handleSevenRooms();
+  prettyLog(
+    `info`,
+    `loaded`,
+    `SCORPIOS | sevenrooms-widget | v.2 | build 09.04.22 @ 13:19`
+  );
+}); // end DOM listener
+
+// initialise sevenrooms for all buttons
+function handleSevenRooms() {
   const buttons = document.querySelectorAll(".btn-widget");
   Array.from(buttons).forEach((button) => {
     const venueId = button.dataset.venue,
       triggerId = button.id;
-    
+
     // inject all venues if needed
     function handleVenues(triggerId) {
       if (venueId == "scorpiosmykonosgroup") {
@@ -20,7 +32,7 @@ document.addEventListener("DOMContentLoaded", () => {
         return [];
       } // end if
     }
-    
+
     // initialise widget
     SevenroomsWidget.init({
       venueId: venueId,
@@ -29,6 +41,39 @@ document.addEventListener("DOMContentLoaded", () => {
       type: "reservations",
       styleButton: false
     }); // end SevenroomsWidget init
-    
   }); // end for each button
-}); // end DOM loaded
+} // end handleSevenRooms()
+
+// helper | log function
+function prettyLog(status, hint, message) {
+  let color;
+  switch (status) {
+    case "info":
+      color = "#3498DB";
+      break;
+    case "success":
+      color = "#27AE60";
+      break;
+    case "warning":
+      color = "#E67E22";
+      break;
+    case "error":
+      color = "#E74C3C";
+      break;
+    default:
+      color = "#90A4AE";
+  }
+  console.log(
+    `%c${hint}`,
+    [
+      `background: ${color}`,
+      `border-radius: 0.5em`,
+      `color: white`,
+      `font-weight: bold`,
+      `padding: 2px 0.5em`
+    ].join(`;`),
+    message
+  );
+} // end prettyLog
+
+// go get an 🍦
